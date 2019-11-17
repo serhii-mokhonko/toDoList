0e9e34f13fd12e93cbde3cfd7e76d0ac975e1e7c
Sergey Mokhonko <sergiy35@gmail.com>
2019-11-17 13:13:08 +0200

    delete task from array and change status of task

--- src/index.js
+++ src/index.js
@@ -1,41 +1,62 @@
 import "./main.css"
 
 const btnAdd = document.getElementById('btn-add');
+const taskContainer = document.getElementById('list');
 
 const todo = [];
 
 btnAdd.addEventListener('click', (e) => {
+    e.preventDefault();
+    //get data from form
     let title = document.getElementById('title').value,
         description = document.getElementById('description').value;
-
-    e.preventDefault();
-
+    //get id for next element
+    let id = todo.reduce((ac, current) => {
+        return ac = Math.max(ac, current.id);
+    }, 0);
+    id++;
     //check input and textarea
     if (title.length > 0 && description.length > 0) {
-
+        //put new element into array
         todo.unshift({
-            title: title,
-            description: description,
+            id,
+            title,
+            description,
             priority: false,
             done: false,
         });
+        //clear form
         document.getElementById('title').value = '';
         document.getElementById('description').value = '';
     } else {
         return;
     }
 
-    //clear container before update
-    const taskContainer = document.getElementById('list')
-    taskContainer.innerHTML = '';
-
-    //show all task
-    for (let task in todo) {
-        const newEl = createTask(todo[task]);
-        taskContainer.appendChild(newEl);
-    }
+    //show all tasks
+    loadAllTasks(todo);
 });
 
+taskContainer.addEventListener('click', (e) => {
+    if (e.target.classList.contains('btn--done')) {
+        for(let item in todo){
+            if(todo[item].id === Number(e.target.getAttribute("id"))){
+               todo[item].done = true;
+            }
+        }
+    }
+
+    if (e.target.classList.contains('btn--del')) {
+        if (e.target.classList.contains('btn--del')) {
+            for(let item in todo){
+                if(todo[item].id === Number(e.target.getAttribute("id"))){
+                   todo.splice(item, 1);
+                }
+            }
+        }
+        loadAllTasks(todo);
+        console.log(todo);
+    }
+});
 
 function createTask(task) {
     const parent = document.createElement('div');
@@ -48,11 +69,20 @@ function createTask(task) {
         <p>${task.description}</p>
     </div>
     <div class="todo-card__actions">
-        <button class="btn btn--done">Done</button>
-        <button class="btn btn--del">Del</button>
+        <button class="btn btn--done" id='${task.id}'>Done</button>
+        <button class="btn btn--del" id='${task.id}'>Del</button>
     </div>`;
 
     parent.innerHTML = html;
 
     return parent;
+}
+
+function loadAllTasks (arr) {
+    //clear container before update
+    taskContainer.innerHTML = '';
+    for (let task in arr) {
+        const newEl = createTask(arr[task]);
+        taskContainer.appendChild(newEl);
+    }
 }
